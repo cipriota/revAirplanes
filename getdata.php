cc831ec21e344b8500311a556d405634211504b6
Andre Savva <cipriota@revMacbookPro.local>
2013-06-16 12:18:22 +0100

    Dev update pic preview

--- getdata.php
+++ getdata.php
@@ -1,4 +1,5 @@
 <?php
-	$json = file_get_contents("http://cipriota.dns-dns.com:8080/data.json");
-	echo $json;
+	//echo file_get_contents("http://localhost:8080/data.json");
+	//echo file_get_contents("http://raspberrypi:8080/data.json");
+	echo file_get_contents("http://cipriota.dns-dns.com:8080/data.json");
 ?>
