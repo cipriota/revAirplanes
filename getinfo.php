5afd4fa68b3f54009615b14564183b3340a02329
Andre Savva <cipriota@revmacbookpro.home>
2013-06-16 02:29:08 +0100

    Added db lookup & aircraft pic

--- getinfo.php
+++ getinfo.php
@@ -1,10 +1,25 @@
 <?php
+	// Aircraft type;
+	//
+	$hex = $_GET['modeS'];
+
+	if ($hex==null)
+		die;
+		
 	try {
-		$dbh = new PDO('sqlite:StandingData.sqb');
+		$dbh = new PDO('sqlite:/db/basestation.sqb');
 	
-		foreach ($dbh->query('SELECT * FROM AircraftType') as $row) {
-    		print_r($row);
+		/*foreach ($dbh->query('SELECT registration FROM aircraft where modeS like \'4950F5\'') as $row) {
+    		print_r($row['Registration']);
   		}
+  		*/
+  		$st = $dbh->query('SELECT registration, OperatorFlagCode FROM aircraft where modeS like \''.$hex.'\'');
+  		
+  		$st->setFetchMode(PDO::FETCH_OBJ);
+  		
+  		$result = $st->fetch();
+  		
+  		print "{\"modeS\": \"" . $hex . "\",\"registration\": \"" . $result->Registration . "\",\"airline\": \"".$result->OperatorFlagCode."\" }";
   		$dbh = null;
 	} catch (PDOException $e) {
 		print "Error!: " . $e->getMessage() . "<br/>";
